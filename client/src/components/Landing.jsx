bc785c93f7b3eb345d5bcad67b888f5ee0f20577
Habiburrahman Dehzad <rhabiblv@gmail.com>
2021-01-19 23:28:42 +0430

    Fixed the landing page for logged in user

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
+import { connect } from 'react-redux';
 
-const Landing = () => {
+const Landing = ({ auth }) => {
   return (
     <Jumbotron className='text-center'>
       <h1>Eamily</h1>
       <p className='lead'>Know your customers.</p>
       <p>
-        Collect feedback from your customers with ease. All you need is a Google
-        account.
+        Collect feedback from your customers with ease.
+        {auth && ' All you need is a Google account'}.
       </p>
-      <p>Click Sign in With Google to begin.</p>
-      <p>
-        <Button variant='primary' href='/auth/google'>
-          Sign in With Google
-        </Button>
-      </p>
+      {auth && (
+        <Fragment>
+          <p>Click Sign in With Google to begin.</p>
+          <p>
+            <Button variant='primary' href='/auth/google'>
+              Sign in With Google
+            </Button>
+          </p>
+        </Fragment>
+      )}
     </Jumbotron>
   );
 };
 
-export default Landing;
+const mapPropsToState = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapPropsToState)(Landing);
