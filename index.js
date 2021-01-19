4960696e1d96e67c91398cff9a787106e7a87725
Habiburrahman Dehzad <rhabiblv@gmail.com>
2021-01-19 22:05:37 +0430

    Prepared for deployment to Heroku

--- index.js
+++ index.js
@@ -30,13 +30,14 @@ mongoose.connect(keys.mongoUri, {
   useUnifiedTopology: true,
 });
 
-app.get('/', (req, res) => {
-  res.send({ Home: 'Emaily home page' });
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-app.get('/error', (req, res) => {
-  res.send(JSON.stringify(req));
-});
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
